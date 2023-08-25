a454ffe8e07f8ac21e122a5b52442b56ae90f57e
Biruk Kalewold <birukkalewoldasrat@gmail.com>
2023-08-26 00:43:40 +0300

    added identification of files and folders

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 import fs from "fs";
 
-fs.readdir(process.cwd(), (err, files) => {
+const { lstat } = fs.promises; //destructore the promisified version of lstat function from fs promises API
+
+fs.readdir(process.cwd(), async (err, files) => {
+  //throw error if failed to read files
   if (err) {
     throw new Error(err.message);
   }
 
-  console.log(files);
+  //find the stats (atributes of each file)
+  const statPromises = files.map((file) => {
+    return lstat(file);
+  });
+
+  try {
+    const fileStats = await Promise.all(statPromises);
+    fileStats.forEach((fileStat, index) => {
+      console.log(files[index], fileStat.isFile());
+    });
+  } catch (err) {
+    throw new Error("failed analysing file/folder");
+  }
 });
